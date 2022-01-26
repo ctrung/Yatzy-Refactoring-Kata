d728a09c55b1a380e0d4e7157e4c2796dc94e628
Clement Trung <clement.trung@axionesi.net>
2022-01-26 19:40:25 +0100

    Refactoring : Renommage et formatage
    - renommage camelCase (code main)
    - renommage snakeCase + description (code test)
    - formatage auto d'Intellij IDEA

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -9,14 +9,14 @@ public class Yatzy {
 
     /**
      * Initialisation de l'état des 5 dés.
+     *
      * @param d1 dé 1.
      * @param d2 dé 2.
      * @param d3 dé 3.
      * @param d4 dé 4.
      * @param d5 dé 5.
      */
-    public Yatzy(int d1, int d2, int d3, int d4, int d5)
-    {
+    public Yatzy(int d1, int d2, int d3, int d4, int d5) {
         dice = new int[5];
         dice[0] = d1;
         dice[1] = d2;
@@ -27,22 +27,22 @@ public class Yatzy {
 
     /**
      * Les joueurs doivent obtenir le plus grand nombre de points / ils marquent la somme de la valeur des dés.
+     *
      * @return la somme des valeurs des dés.
      */
-    public int chance()
-    {
+    public int chance() {
         return dice[0] + dice[1] + dice[2] + dice[3] + dice[4];
     }
 
     /**
      * Les joueurs doivent obtenir 5 dés de même valeur / ils marquent 50 points.
+     *
      * @return 0 ou 50 points selon qu'il y ait 5 dés de même valeur.
      */
-    public int yatzy()
-    {
+    public int yatzy() {
         int[] counts = new int[6];
         for (int die : dice)
-            counts[die-1]++;
+            counts[die - 1]++;
         for (int i = 0; i != 6; i++)
             if (counts[i] == 5)
                 return 50;
@@ -52,6 +52,7 @@ public class Yatzy {
     /**
      * Partie de 1 : les joueurs doivent obtenir le plus grand nombre de dés avec la face 1 /
      * ils marquent 1 fois le nombre de dés de valeur 1.
+     *
      * @return Un score entre 0 et 5 en fonction du nombre de dés avec la face 1.
      */
     public int ones() {
@@ -69,6 +70,7 @@ public class Yatzy {
     /**
      * Partie de 2 : les joueurs doivent obtenir le plus grand nombre de dés avec la face 2 /
      * ils marquent 2 fois le nombre de dés de valeur 2.
+     *
      * @return Un score entre 0 et 10 en fonction du nombre de dés avec la face 2.
      */
     public int twos() {
@@ -84,6 +86,7 @@ public class Yatzy {
     /**
      * Partie de 3 : les joueurs doivent obtenir le plus grand nombre de dés avec la face 3 /
      * ils marquent 3 fois le nombre de dés de valeur 3.
+     *
      * @return Un score entre 0 et 15 en fonction du nombre de dés avec la face 3.
      */
     public int threes() {
@@ -100,11 +103,11 @@ public class Yatzy {
     /**
      * Partie de 4 : les joueurs doivent obtenir le plus grand nombre de dés avec la face 4 /
      * ils marquent 4 fois le nombre de dés de valeur 4.
+     *
      * @return Un score entre 0 et 20 en fonction du nombre de dés avec la face 4.
      */
-    public int fours()
-    {
-        int sum;    
+    public int fours() {
+        int sum;
         sum = 0;
         for (int at = 0; at != 5; at++) {
             if (dice[at] == 4) {
@@ -117,13 +120,13 @@ public class Yatzy {
     /**
      * Partie de 5 : les joueurs doivent obtenir le plus grand nombre de dés avec la face 5 /
      * ils marquent 5 fois le nombre de dés de valeur 5.
+     *
      * @return Un score entre 0 et 25 en fonction du nombre de dés avec la face 5.
      */
-    public int fives()
-    {
+    public int fives() {
         int s = 0;
         int i;
-        for (i = 0; i < dice.length; i++) 
+        for (i = 0; i < dice.length; i++)
             if (dice[i] == 5)
                 s = s + 5;
         return s;
@@ -132,12 +135,12 @@ public class Yatzy {
     /**
      * Partie de 6 : les joueurs doivent obtenir le plus grand nombre de dés avec la face 6 /
      * ils marquent 6 fois le nombre de dés de valeur 6.
+     *
      * @return Un score entre 0 et 30 en fonction du nombre de dés avec la face 6.
      */
-    public int sixes()
-    {
+    public int sixes() {
         int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
+        for (int at = 0; at < dice.length; at++)
             if (dice[at] == 6)
                 sum = sum + 6;
         return sum;
@@ -146,43 +149,43 @@ public class Yatzy {
     /**
      * Partie de paires : les joueurs doivent obtenir la plus grande paire /
      * ils marquent la valeur de la plus grande paire (ex. paire de 6 = 12, paire de 5 = 10, etc...)
+     *
      * @return La valeur de la plus grande paire si présente, 0 sinon.
      */
-    public int score_pair()
-    {
+    public int onePair() {
         int[] counts = new int[6];
-        counts[dice[0]-1]++;
-        counts[dice[1]-1]++;
-        counts[dice[2]-1]++;
-        counts[dice[3]-1]++;
-        counts[dice[4]-1]++;
+        counts[dice[0] - 1]++;
+        counts[dice[1] - 1]++;
+        counts[dice[2] - 1]++;
+        counts[dice[3] - 1]++;
+        counts[dice[4] - 1]++;
         int at;
         for (at = 0; at != 6; at++)
-            if (counts[6-at-1] >= 2)
-                return (6-at)*2;
+            if (counts[6 - at - 1] >= 2)
+                return (6 - at) * 2;
         return 0;
     }
 
     /**
      * Partie de doubles paires : les joueurs doivent obtenir deux paires /
      * ils marquent la valeur des deux paires (ex. paire de 3 et 5 = 16, etc...)
+     *
      * @return La valeur des deux paires cumulées si présentes, 0 sinon.
      */
-    public int two_pair()
-    {
+    public int twoPairs() {
         int[] counts = new int[6];
-        counts[dice[0]-1]++;
-        counts[dice[1]-1]++;
-        counts[dice[2]-1]++;
-        counts[dice[3]-1]++;
-        counts[dice[4]-1]++;
+        counts[dice[0] - 1]++;
+        counts[dice[1] - 1]++;
+        counts[dice[2] - 1]++;
+        counts[dice[3] - 1]++;
+        counts[dice[4] - 1]++;
         int n = 0;
         int score = 0;
         for (int i = 0; i < 6; i += 1)
-            if (counts[6-i-1] >= 2) {
+            if (counts[6 - i - 1] >= 2) {
                 n++;
-                score += (6-i);
-            }        
+                score += (6 - i);
+            }
         if (n == 2)
             return score * 2;
         else
@@ -191,55 +194,55 @@ public class Yatzy {
 
     /**
      * Carré : les joueurs doivent obtenir 4 dés de même valeur / ils marquent 4 fois la valeur des dés identiques.
+     *
      * @return 4 fois la valeur des dés identiques en présence d'un carré, 0 sinon.
      */
-    public int four_of_a_kind()
-    {
+    public int fourOfAKind() {
         int[] tallies;
         tallies = new int[6];
-        tallies[dice[0]-1]++;
-        tallies[dice[1]-1]++;
-        tallies[dice[2]-1]++;
-        tallies[dice[3]-1]++;
-        tallies[dice[4]-1]++;
+        tallies[dice[0] - 1]++;
+        tallies[dice[1] - 1]++;
+        tallies[dice[2] - 1]++;
+        tallies[dice[3] - 1]++;
+        tallies[dice[4] - 1]++;
         for (int i = 0; i < 6; i++)
             if (tallies[i] >= 4)
-                return (i+1) * 4;
+                return (i + 1) * 4;
         return 0;
     }
 
     /**
      * Brelan : les joueurs doivent obtenir 3 dés de même valeur / ils marquent 3 fois la valeur des dés identiques.
+     *
      * @return 3 fois la valeur des dés identiques en présence d'un brelan, 0 sinon.
      */
-    public int three_of_a_kind()
-    {
+    public int threeOfAKind() {
         int[] t;
         t = new int[6];
-        t[dice[0]-1]++;
-        t[dice[1]-1]++;
-        t[dice[2]-1]++;
-        t[dice[3]-1]++;
-        t[dice[4]-1]++;
+        t[dice[0] - 1]++;
+        t[dice[1] - 1]++;
+        t[dice[2] - 1]++;
+        t[dice[3] - 1]++;
+        t[dice[4] - 1]++;
         for (int i = 0; i < 6; i++)
             if (t[i] >= 3)
-                return (i+1) * 3;
+                return (i + 1) * 3;
         return 0;
     }
 
     /**
      * Petite suite : les joueurs doivent obtenir 5 dés qui se suivent jusqu'à 5 (1-2-3-4-5) / ils marquent 15 points.
+     *
      * @return 15 points si présence d'une petite suite, 0 sinon.
      */
-    public int smallStraight()
-    {
+    public int smallStraight() {
         int[] tallies;
         tallies = new int[6];
-        tallies[dice[0]-1] += 1;
-        tallies[dice[1]-1] += 1;
-        tallies[dice[2]-1] += 1;
-        tallies[dice[3]-1] += 1;
-        tallies[dice[4]-1] += 1;
+        tallies[dice[0] - 1] += 1;
+        tallies[dice[1] - 1] += 1;
+        tallies[dice[2] - 1] += 1;
+        tallies[dice[3] - 1] += 1;
+        tallies[dice[4] - 1] += 1;
         if (tallies[0] == 1 &&
             tallies[1] == 1 &&
             tallies[2] == 1 &&
@@ -251,17 +254,17 @@ public class Yatzy {
 
     /**
      * Grande suite : les joueurs doivent obtenir 5 dés qui se suivent jusqu'à 6 (2-3-4-5-6) / ils marquent 20 points.
+     *
      * @return 20 points si présence d'une grance suite, 0 sinon.
      */
-    public int largeStraight()
-    {
+    public int largeStraight() {
         int[] tallies;
         tallies = new int[6];
-        tallies[dice[0]-1] += 1;
-        tallies[dice[1]-1] += 1;
-        tallies[dice[2]-1] += 1;
-        tallies[dice[3]-1] += 1;
-        tallies[dice[4]-1] += 1;
+        tallies[dice[0] - 1] += 1;
+        tallies[dice[1] - 1] += 1;
+        tallies[dice[2] - 1] += 1;
+        tallies[dice[3] - 1] += 1;
+        tallies[dice[4] - 1] += 1;
         if (tallies[1] == 1 &&
             tallies[2] == 1 &&
             tallies[3] == 1 &&
@@ -274,10 +277,10 @@ public class Yatzy {
     /**
      * Les joueurs doivent obtenir 3 dés de même valeur et 2 dés de même valeur – (brelan + paire) /
      * ils marquent la somme des valeurs des dés.
+     *
      * @return La somme des valeurs de tous les dés si présence d'un full house, 0 sinon.
      */
-    public int fullHouse()
-    {
+    public int fullHouse() {
         int[] tallies;
         boolean _2 = false;
         int i;
@@ -286,25 +289,23 @@ public class Yatzy {
         int _3_at = 0;
 
 
-
-
         tallies = new int[6];
-        tallies[dice[0]-1] += 1;
-        tallies[dice[1]-1] += 1;
-        tallies[dice[2]-1] += 1;
-        tallies[dice[3]-1] += 1;
-        tallies[dice[4]-1] += 1;
+        tallies[dice[0] - 1] += 1;
+        tallies[dice[1] - 1] += 1;
+        tallies[dice[2] - 1] += 1;
+        tallies[dice[3] - 1] += 1;
+        tallies[dice[4] - 1] += 1;
 
         for (i = 0; i != 6; i += 1)
             if (tallies[i] == 2) {
                 _2 = true;
-                _2_at = i+1;
+                _2_at = i + 1;
             }
 
         for (i = 0; i != 6; i += 1)
             if (tallies[i] == 3) {
                 _3 = true;
-                _3_at = i+1;
+                _3_at = i + 1;
             }
 
         if (_2 && _3)
--- java/src/test/java/YatzyTest.java
+++ java/src/test/java/YatzyTest.java
@@ -1,4 +1,5 @@
 import org.junit.*;
+
 import static org.junit.Assert.*;
 
 public class YatzyTest {
@@ -6,105 +7,105 @@ public class YatzyTest {
     @Test
     public void chance_scores_sum_of_all_dice() {
         int expected = 15;
-        int actual = new Yatzy(2,3,4,5,1).chance();
+        int actual = new Yatzy(2, 3, 4, 5, 1).chance();
         assertEquals(expected, actual);
-        assertEquals(16, new Yatzy(3,3,4,5,1).chance());
+        assertEquals(16, new Yatzy(3, 3, 4, 5, 1).chance());
     }
 
-    @Test public void yatzy_scores_50() {
+    @Test
+    public void yatzy_scores_50() {
         int expected = 50;
-        int actual = new Yatzy(4,4,4,4,4).yatzy();
+        int actual = new Yatzy(4, 4, 4, 4, 4).yatzy();
         assertEquals(expected, actual);
-        assertEquals(50, new Yatzy(6,6,6,6,6).yatzy());
-        assertEquals(0, new Yatzy(6,6,6,6,3).yatzy());
-    }
-
-    @Test public void test_1s() {
-        assertTrue(new Yatzy(1,2,3,4,5).ones() == 1);
-        assertEquals(2, new Yatzy(1,2,1,4,5).ones());
-        assertEquals(0, new Yatzy(6,2,2,4,5).ones());
-        assertEquals(4, new Yatzy(1,2,1,1,1).ones());
+        assertEquals(50, new Yatzy(6, 6, 6, 6, 6).yatzy());
+        assertEquals(0, new Yatzy(6, 6, 6, 6, 3).yatzy());
     }
 
     @Test
-    public void test_2s() {
-        assertEquals(4, new Yatzy(1,2,3,2,6).twos());
-        assertEquals(10, new Yatzy(2,2,2,2,2).twos());
+    public void ones_scores_the_number_of_one() {
+        assertTrue(new Yatzy(1, 2, 3, 4, 5).ones() == 1);
+        assertEquals(2, new Yatzy(1, 2, 1, 4, 5).ones());
+        assertEquals(0, new Yatzy(6, 2, 2, 4, 5).ones());
+        assertEquals(4, new Yatzy(1, 2, 1, 1, 1).ones());
     }
 
     @Test
-    public void test_threes() {
-        assertEquals(6, new Yatzy(1,2,3,2,3).threes());
-        assertEquals(12, new Yatzy(2,3,3,3,3).threes());
+    public void twos_scores_two_times_the_number_of_two() {
+        assertEquals(4, new Yatzy(1, 2, 3, 2, 6).twos());
+        assertEquals(10, new Yatzy(2, 2, 2, 2, 2).twos());
     }
 
     @Test
-    public void fours_test() 
-    {
-        assertEquals(12, new Yatzy(4,4,4,5,5).fours());
-        assertEquals(8, new Yatzy(4,4,5,5,5).fours());
-        assertEquals(4, new Yatzy(4,5,5,5,5).fours());
+    public void threes_scores_three_times_the_number_of_three() {
+        assertEquals(6, new Yatzy(1, 2, 3, 2, 3).threes());
+        assertEquals(12, new Yatzy(2, 3, 3, 3, 3).threes());
     }
 
     @Test
-    public void fives() {
-        assertEquals(10, new Yatzy(4,4,4,5,5).fives());
-        assertEquals(15, new Yatzy(4,4,5,5,5).fives());
-        assertEquals(20, new Yatzy(4,5,5,5,5).fives());
+    public void fours_scores_four_times_the_number_of_four() {
+        assertEquals(12, new Yatzy(4, 4, 4, 5, 5).fours());
+        assertEquals(8, new Yatzy(4, 4, 5, 5, 5).fours());
+        assertEquals(4, new Yatzy(4, 5, 5, 5, 5).fours());
     }
 
     @Test
-    public void sixes_test() {
-        assertEquals(0, new Yatzy(4,4,4,5,5).sixes());
-        assertEquals(6, new Yatzy(4,4,6,5,5).sixes());
-        assertEquals(18, new Yatzy(6,5,6,6,5).sixes());
+    public void fives_scores_five_times_the_number_of_five() {
+        assertEquals(10, new Yatzy(4, 4, 4, 5, 5).fives());
+        assertEquals(15, new Yatzy(4, 4, 5, 5, 5).fives());
+        assertEquals(20, new Yatzy(4, 5, 5, 5, 5).fives());
     }
 
     @Test
-    public void one_pair() {
-        assertEquals(6, new Yatzy(3, 4, 3, 5, 6).score_pair());
-        assertEquals(10, new Yatzy(5,3,3,3,5).score_pair());
-        assertEquals(12, new Yatzy(5,3,6,6,5).score_pair());
+    public void sixes_scores_six_times_the_number_of_six() {
+        assertEquals(0, new Yatzy(4, 4, 4, 5, 5).sixes());
+        assertEquals(6, new Yatzy(4, 4, 6, 5, 5).sixes());
+        assertEquals(18, new Yatzy(6, 5, 6, 6, 5).sixes());
     }
 
     @Test
-    public void two_Pair() {
-        assertEquals(16, new Yatzy(3,3,5,4,5).two_pair());
-        assertEquals(16, new Yatzy(3,3,5,5,5).two_pair());
+    public void one_pair_scores_the_value_of_the_greatest_pair() {
+        assertEquals(6, new Yatzy(3, 4, 3, 5, 6).onePair());
+        assertEquals(10, new Yatzy(5, 3, 3, 3, 5).onePair());
+        assertEquals(12, new Yatzy(5, 3, 6, 6, 5).onePair());
     }
 
     @Test
-    public void three_of_a_kind() 
-    {
-        assertEquals(9, new Yatzy(3,3,3,4,5).three_of_a_kind());
-        assertEquals(15, new Yatzy(5,3,5,4,5).three_of_a_kind());
-        assertEquals(9, new Yatzy(3,3,3,3,5).three_of_a_kind());
+    public void two_pairs_scores_the_value_of_two_pairs() {
+        assertEquals(16, new Yatzy(3, 3, 5, 4, 5).twoPairs());
+        assertEquals(16, new Yatzy(3, 3, 5, 5, 5).twoPairs());
     }
 
     @Test
-    public void four_of_a_knd() {
-        assertEquals(12, new Yatzy(3,3,3,3,5).four_of_a_kind());
-        assertEquals(20, new Yatzy(5,5,5,4,5).four_of_a_kind());
-        assertEquals(9, new Yatzy(3,3,3,3,3).three_of_a_kind());
+    public void three_of_a_kind_scores_the_sum_of_three_equal_dices() {
+        assertEquals(9, new Yatzy(3, 3, 3, 4, 5).threeOfAKind());
+        assertEquals(15, new Yatzy(5, 3, 5, 4, 5).threeOfAKind());
+        assertEquals(9, new Yatzy(3, 3, 3, 3, 5).threeOfAKind());
     }
 
     @Test
-    public void smallStraight() {
-        assertEquals(15, new Yatzy(1,2,3,4,5).smallStraight());
-        assertEquals(15, new Yatzy(2,3,4,5,1).smallStraight());
-        assertEquals(0, new Yatzy(1,2,2,4,5).smallStraight());
+    public void four_of_a_kind_scores_the_sum_of_four_equal_dices() {
+        assertEquals(12, new Yatzy(3, 3, 3, 3, 5).fourOfAKind());
+        assertEquals(20, new Yatzy(5, 5, 5, 4, 5).fourOfAKind());
+        assertEquals(9, new Yatzy(3, 3, 3, 3, 3).threeOfAKind());
     }
 
     @Test
-    public void largeStraight() {
-        assertEquals(20, new Yatzy(6,2,3,4,5).largeStraight());
-        assertEquals(20, new Yatzy(2,3,4,5,6).largeStraight());
-        assertEquals(0, new Yatzy(1,2,2,4,5).largeStraight());
+    public void small_straight_scores_15_if_one_to_five_present() {
+        assertEquals(15, new Yatzy(1, 2, 3, 4, 5).smallStraight());
+        assertEquals(15, new Yatzy(2, 3, 4, 5, 1).smallStraight());
+        assertEquals(0, new Yatzy(1, 2, 2, 4, 5).smallStraight());
     }
 
     @Test
-    public void fullHouse() {
-        assertEquals(18, new Yatzy(6,2,2,2,6).fullHouse());
-        assertEquals(0, new Yatzy(2,3,4,5,6).fullHouse());
+    public void large_straight_scores_20_if_two_to_six_present() {
+        assertEquals(20, new Yatzy(6, 2, 3, 4, 5).largeStraight());
+        assertEquals(20, new Yatzy(2, 3, 4, 5, 6).largeStraight());
+        assertEquals(0, new Yatzy(1, 2, 2, 4, 5).largeStraight());
+    }
+
+    @Test
+    public void full_house_scores_the_sum_of_one_pair_and_three_of_a_kind() {
+        assertEquals(18, new Yatzy(6, 2, 2, 2, 6).fullHouse());
+        assertEquals(0, new Yatzy(2, 3, 4, 5, 6).fullHouse());
     }
 }
